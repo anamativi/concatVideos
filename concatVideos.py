cbdf74a6d1a1fa25c724e21696f33da6f6a2ffea
anamativi <acmativi@hotmail.com>
2015-07-14 16:37:42 -0300

    Added comments and chaged var names

--- concatVideos.py
+++ concatVideos.py
@@ -1,9 +1,9 @@
 import sys
 
-proj = sys.argv[1]
+projectFile = sys.argv[1]
 GOPSize = sys.argv[2]
 
-r = open("result_concatVideos.yuv", 'wb')
+result = open("concatVideos" + projectFile + ".yuv", 'wb')
 p = open(proj + ".txt", "r")
 lines = p.readlines()
 p.close()
@@ -11,17 +11,19 @@ p.close()
 	
 def copyGOP(video, h, w, startFrame):
 	f = open("../../origCfP/" + video + ".yuv", 'rb')
-	frame = 3 * (int(h) * int(w)) / 2
+	frame = 3 * (int(h) * int(w)) / 2 #YCrCb
 	
-	for frames in range(0,int(startFrame)):
+	for frames in range(0,int(startFrame)): #reads frames before the starting frame
 		f.read(frame)
-	for frames in range(int(startFrame), (int(startFrame) + int(GOPSize))):
-		string = f.read(frame)
-		r.write(string)
+	for frames in range(int(startFrame), (int(startFrame) + int(GOPSize))): #reads and saves the GOP
+		YUV = f.read(frame)
+		result.write(YUV)
+		
 	f.close()
+	
 for line in lines:
 	line = line.split()
 	print line
 	copyGOP(line[0], line[1], line[2], line[3])
 
-r.close()
+result.close()
