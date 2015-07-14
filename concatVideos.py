78ed056cac7e488f39571de22345fffede79f117
anamativi <acmativi@hotmail.com>
2015-07-14 14:49:18 -0300

    Working!

--- concatVideos.py
+++ concatVideos.py
@@ -6,20 +6,22 @@ GOPSize = sys.argv[2]
 r = open("result_concatVideos.yuv", 'wb')
 p = open(proj + ".txt", "r")
 lines = p.readlines()
-p.close
+p.close()
 
 	
 def copyGOP(video, h, w, startFrame):
-	f = open("../../origCfP/" + video, 'rb')
+	f = open("../../origCfP/" + video + ".yuv", 'rb')
 	frame = 3 * (int(h) * int(w)) / 2
 	
 	for frames in range(0,int(startFrame)):
 		f.read(frame)
 	for frames in range(int(startFrame), (int(startFrame) + int(GOPSize))):
 		string = f.read(frame)
-		f.write(string)
-
+		r.write(string)
+	f.close()
 for line in lines:
 	line = line.split()
 	print line
 	copyGOP(line[0], line[1], line[2], line[3])
+
+r.close()
